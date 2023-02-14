de3163b80eac7d5c0cbdba079a9bc82cabe2eeb1
TestaVuota <77064292+TestaVuota@users.noreply.github.com>
2023-02-14 08:27:33 +0100

    Add files via upload

---
+++ best_practice2.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  7 18:56:10 2022
+
+@author: nicol
+"""
+
+from time import sleep
+
+print("This is my file to demonstrate best practices")
+
+def process_data(data):
+    print("Beginning data processing ...")
+    modified_data = data +"Tha has been modified"
+    sleep(3)
+    print('Data processing finished.')
+    return modified_data
+
+if __name__ == "__main__":
+    data = "My data read from the web"
+    print(data)
+    modified_data = process_data(data)
+    print(modified_data)
